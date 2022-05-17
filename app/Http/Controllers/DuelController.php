97fa8d408b1df03af1739307584a68a563586b2b
Takimekufu <sub7ziro7maks7@gmail.com>
2022-05-17 21:33:58 +0700

    fix rating and add cursorPagination

--- app/Http/Controllers/DuelController.php
+++ app/Http/Controllers/DuelController.php
@@ -14,24 +14,26 @@ class DuelController extends Controller
 {
     public function showAllDuels(){ // вывести всю таблицу Duels
         //return response()->json(Duel::get()); 
-        return DuelResource::collection(Duel::orderByDesc('id')->paginate());
+        return DuelResource::collection(Duel::orderByDesc('id')->cursorPaginate());
     }
-
+    /*
     public function showPlayerDuelsID(Player $player){ // вывести id матчей в которых участвовал игрок
         return response()->json(Duel::select('id')->where('id_first', $player->id)->orwhere('id_second', $player->id)->get()); 
         //return $player->showDuelsId();
     }
+    */
 
     public function showPlayerDuelsInfo(Player $player){ // вывести информацию о матчах в которых участвовал игрок
         //return response()->json(Duel::where('id_first', $player->id)->orwhere('id_second', $player->id)->get()); 
-        return DuelResource::collection(Duel::where('id_first', $player->id)->orwhere('id_second', $player->id)->orderByDesc('id')->paginate());
+        return DuelResource::collection(Duel::where('id_first', $player->id)->orwhere('id_second', $player->id)->orderByDesc('id')->cursorPaginate());
     }
 
-
+    /*
     public function showTournamentDuelsID(Tournament $tournament){ // вывести id матчей турнира
         return response()->json(Duel::select('id')->where('id_tournament', $tournament->id)->get()); 
     }
-
+    */ 
+     
     public function showTournamentDuelsInfo(Tournament $tournament){ // вывести информацию о матчах турнира
         //return response()->json(Duel::where('id_tournament', $tournament->id)->get()); 
         return DuelResource::collection(Duel::where('id_tournament', $tournament->id)->orderByDesc('id')->get());
@@ -46,14 +48,14 @@ class DuelController extends Controller
         $id_first = $duel['id_first'];
         $id_second = $duel['id_second'];
 
-        $rating_first = Player::find($id_first)->rating;
-        $rating_second = Player::find($id_second)->rating;
+        $rating_first_for_duel = Player::find($id_first)->rating;
+        $rating_second_for_duel = Player::find($id_second)->rating;
 
         
         if ($duel['score_first'] > $duel['score_second']){
-            $temprating = (100 - ($rating_first - $rating_second))/10;
-            $rating_first = $rating_first + $temprating;
-            $rating_second = $rating_second - $temprating;
+            $delta_rating = (100 - ($rating_first_for_duel - $rating_second_for_duel))/10;
+            $rating_first = $rating_first_for_duel + $delta_rating;
+            $rating_second = $rating_second_for_duel - $delta_rating;
 
             if($rating_second < 1){
                 $rating_second = 1;
@@ -73,9 +75,9 @@ class DuelController extends Controller
                 'rating' => $rating_second
             ]);
         } elseif($duel['score_first'] < $duel['score_second']) {
-            $temprating = (100 - ($rating_second - $rating_first))/10;
-            $rating_second = $rating_second + $temprating;
-            $rating_first = $rating_first - $temprating;
+            $delta_rating = (100 - ($rating_second_for_duel - $rating_first_for_duel))/10;
+            $rating_second = $rating_second_for_duel + $delta_rating;
+            $rating_first = $rating_first_for_duel - $delta_rating;
 
             if($rating_first < 1){
                 $rating_first = 1;
@@ -104,8 +106,8 @@ class DuelController extends Controller
             'id_second' => $id_second,
             'score_first' => $duel['score_first'],
             'score_second' => $duel['score_second'],
-            'rating_first' => $rating_first,
-            'rating_second' => $rating_second,
+            'rating_first' => $rating_first_for_duel,
+            'rating_second' => $rating_second_for_duel,
             'id_tournament' => $id_tournament,
             'index_duel' => $duel['index_duel']
         ]); 
